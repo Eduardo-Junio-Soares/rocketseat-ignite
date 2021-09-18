fc8d8a6fc51bf18ba351445e04e0877c4c5d5fe3
eduardo <eduardo.soares@acct.global>
2021-09-18 15:26:19 -0300

    first commit

---
+++ ReactJS/01-github-explorer/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babbel/preset-typescript'
+        ['@babel/preset-react', {
+            runtime: 'automatic'
+        }]
+    ]
+};
---
+++ ReactJS/01-github-explorer/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const ReactRefleshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        hot: true,
+    },
+    plugins: [
+        isDevelopment && new ReactRefleshWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+        
+    ].filter(Boolean),
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node-modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+};
