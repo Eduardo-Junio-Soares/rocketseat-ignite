fb83f11f764c1919db1eaa3063c88a0fd3dbccac
eduardo <eduardo.soares@acct.global>
2021-09-23 20:25:54 -0300

    added components

---
+++ ReactJS/dtmoney/src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+
+export const Container = styled.header `
+    background: var(--blue);
+`;
+
+export const Content = styled.div `
+    margin: 0 auto;
+    max-width: 1120px;
+
+    padding: 2rem 1rem 10rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+        font-size: 1rem;
+        color: #fff;
+        background: var(--blue-light);
+        padding: 0 2rem;
+        border-radius: 0.25rem;
+        height: 3rem;
+        border: 0;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9)
+        }
+    }
+`;
